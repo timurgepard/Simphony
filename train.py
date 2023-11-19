626db14829387abfdce6e9681afc5566434f5445
timurgepard <timur-ishuov@rambler.ru>
2023-11-19 15:17:48 +0100

    Update train.py

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ import numpy as np
 import gymnasium as gym
 import pickle
 import time
-from symphony_ver_2_0 import Symphony, ReplayBuffer
+from symphony_op_2 import Symphony, ReplayBuffer
 import math
 
 
