82411d5d99b5e056604b8588a001fd6ab658532f
timurgepard <timur-ishuov@rambler.ru>
2024-01-29 14:48:25 +0600

    Update symphony_op_2.py

--- symphony_op_2.py
+++ symphony_op_2.py
@@ -73,15 +73,15 @@ class Actor(nn.Module):
         )
 
         self.max_action = torch.mean(max_action).item()
-        if not noiseless: self.max_action *= 0.15
+        self.k = 1.0 if noiseless else 0.15
         self.x_coor = 0.0
         self.noiseless = noiseless
     
     def noise(self, x):
         if self.noiseless and self.x_coor>=math.pi: return 0.0
-        if not self.noiseless and self.x_coor>=2.498: return (0.03*torch.randn_like(x)).clamp(-0.075, 0.075)
+        if not self.noiseless and self.x_coor>=2.133: return (0.07*torch.randn_like(x)).clamp(-0.175, 0.175)
         with torch.no_grad():
-            eps = self.max_action * (math.cos(self.x_coor) + 1.0)
+            eps = self.k * self.max_action * (math.cos(self.x_coor) + 1.0)
             lim = 2.5*eps
             self.x_coor += 3e-5
         return (eps*torch.randn_like(x)).clamp(-lim, lim)
@@ -269,4 +269,3 @@ class ReplayBuffer:
 
     def __len__(self):
         return self.length
-
