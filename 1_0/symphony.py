0d2c3badef941293c71818174b292ef0c55aa218
timurgepard <timur-ishuov@rambler.ru>
2024-05-31 15:15:32 +0500

    Update symphony.py

--- 1_0/symphony.py
+++ 1_0/symphony.py
@@ -21,7 +21,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # random seeds
 r1, r2, r3 = random.randint(0,2**32-1), random.randint(0,2**32-1), random.randint(0,2**32-1)
-#r1, r2, r3 = 3879633802, 4063783399, 2147118445
+#r1, r2, r3 = 2534946416, 201514968, 855242544
 print(r1, ", ", r2, ", ", r3)
 torch.manual_seed(r1)
 np.random.seed(r2)
@@ -47,25 +47,35 @@ def ReHAE(error):
     e = error.mean()
     return torch.abs(e)*torch.tanh(e)
 
+
 class ReSine(nn.Module):
     def __init__(self, hidden_dim=256):
         super(ReSine, self).__init__()
-        self.scale = nn.Parameter(data=torch.randn(hidden_dim))
+        self.scale = nn.Parameter(data=torch.rand(hidden_dim))
     def forward(self, x):
-        scale = 1.0+0.5*torch.tanh(self.scale)
+        scale = 1.0 + 0.5*torch.tanh(self.scale)
         x = scale*torch.sin(x/scale)
         return F.prelu(x, 0.1*scale)
   
 
-class Spike(nn.Module):
-    def __init__(self, hidden_dim=256):
-        super(Spike, self).__init__()
-        self.lnorm = nn.LayerNorm(hidden_dim)
-        self.scale = nn.Parameter(data=torch.ones(hidden_dim)*math.e)
+
+class InplaceDropout(nn.Module):
+    def __init__(self, hidden_dim, drop_count):
+        super(InplaceDropout, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.drop_count = drop_count
+        self.step = 0
+        
+        # Precompute dropout indices for each step in the cycle
+        self.drop_indices = []
+        self.num_cycles = hidden_dim // drop_count
+        for i in range(self.num_cycles):
+            indices = [i * drop_count + j for j in range(drop_count)]
+            self.drop_indices.append(indices)
 
     def forward(self, x):
-        scale = math.e + torch.abs(self.scale)
-        return  scale*torch.tanh(self.lnorm(x)/scale)
+        x[..., self.drop_indices[random.randint(0,self.num_cycles-1)]] = 0.0
+        return x
 
 
 class FeedForward(nn.Module):
@@ -74,10 +84,13 @@ class FeedForward(nn.Module):
 
         self.ffw = nn.Sequential(
             nn.Linear(f_in, 384),
-            Spike(384),
+            InplaceDropout(384, 24),
+            nn.LayerNorm(384),
             nn.Linear(384, 256),
-            ReSine(),
+            InplaceDropout(256, 16),
+            ReSine(256),
             nn.Linear(256, 128),
+            InplaceDropout(128, 8),
             nn.Linear(128, f_out),
         )
 
@@ -92,15 +105,15 @@ class Actor(nn.Module):
         
         self.inA = nn.Sequential(
             nn.Linear(state_dim, hidden_dim),
-            nn.Dropout(0.05),
+            nn.Dropout(0.05)
         )
         self.inB = nn.Sequential(
             nn.Linear(state_dim, hidden_dim),
-            nn.Dropout(0.05),
+            nn.Dropout(0.05)
         )
         self.inC = nn.Sequential(
             nn.Linear(state_dim, hidden_dim),
-            nn.Dropout(0.05),
+            nn.Dropout(0.05)
         )
         
 
@@ -110,18 +123,17 @@ class Actor(nn.Module):
         )
 
         self.max_action = torch.mean(max_action).item()
+        self.scale = 0.05*self.max_action
+        self.lim = 2.5*self.scale
     
     
     def forward(self, state):
         x = torch.cat([self.inA(state), self.inB(state), self.inC(state)], dim=-1)
-        x = self.ffw(x)
-        return self.max_action*x
+        return self.max_action*self.ffw(x)
     
     def action(self, state, mean=False):
-        with torch.no_grad():
-            x = self.forward(state)
-            if mean: return x
-            x += (0.05*torch.randn_like(x)).clamp(-0.15, 0.15)
+        with torch.no_grad(): x = self.forward(state)
+        x += (self.scale*torch.randn_like(x)).clamp(-self.lim, self.lim)
         return x.clamp(-self.max_action, self.max_action)
 
 
@@ -133,21 +145,9 @@ class Critic(nn.Module):
 
 
 
-        qA = nn.Sequential(
-            FeedForward(state_dim+action_dim, hidden_dim, 10),
-            nn.Dropout(0.5)
-        )
-
-
-        qB = nn.Sequential(
-            FeedForward(state_dim+action_dim, hidden_dim, 10),
-            nn.Dropout(0.5)
-        )
-
-        qC = nn.Sequential( 
-            FeedForward(state_dim+action_dim, hidden_dim, 10),
-            nn.Dropout(0.5)
-        )
+        qA = FeedForward(state_dim+action_dim, hidden_dim, 128)
+        qB = FeedForward(state_dim+action_dim, hidden_dim, 128)
+        qC = FeedForward(state_dim+action_dim, hidden_dim, 128)
 
         self.nets = nn.ModuleList([qA, qB, qC])
 
@@ -158,15 +158,15 @@ class Critic(nn.Module):
         xs = [net(x) for net in self.nets]
         if not united: return xs
         x = torch.cat(xs, dim=-1)
-        x = torch.where(x==0.0, 1e+6, x)
-        return torch.min(x, dim=-1, keepdim=True).values
+        x = torch.sort(x, dim=-1).values
+        return torch.mean(x[...,:-38], dim=-1, keepdim=True)
 
 
 # Define the actor-critic agent
 class Symphony(object):
-    def __init__(self, state_dim, action_dim, hidden_dim, device, max_action=1.0, tau=0.005, fade_factor=5.0, lambda_r=0.02, explore_time=1000):
+    def __init__(self, state_dim, action_dim, hidden_dim, device, max_action=1.0, tau=0.005, capacity=192000, fade_factor=10.0, lambda_r=0.02, explore_time=1000):
 
-        self.replay_buffer = ReplayBuffer(state_dim, action_dim, device, fade_factor, lambda_r, explore_time)
+        self.replay_buffer = ReplayBuffer(state_dim, action_dim, device, capacity, fade_factor, lambda_r, explore_time)
 
 
         self.actor = Actor(state_dim, action_dim, device, hidden_dim, max_action=max_action).to(device)
@@ -185,6 +185,7 @@ class Symphony(object):
         self.state_dim = state_dim
         self.action_dim = action_dim
         self.q_next_old_policy = 0.0
+        self.lambda_r = lambda_r 
 
 
 
@@ -195,45 +196,41 @@ class Symphony(object):
         return action.cpu().data.numpy().flatten()
 
 
-    def train(self, tr_per_step=1):
-        #Update-To-Data (UTD)
-        for _ in range(tr_per_step):
+    def train(self, tr_per_step=10):
+        for i in range(tr_per_step):
             state, action, reward, next_state, done = self.replay_buffer.sample()
-            #Sample Multiple Reuse (SMR)
-            for _ in range(3):
-                self.update(state, action, reward, next_state, done)
-                #self.actor_update(state)
+            self.update(state, action, reward, next_state, done)
+
             
 
 
     def update(self, state, action, reward, next_state, done):
 
-        #Actor Update
-
         next_action = self.actor(next_state)
-        q_next_target = self.critic_target(next_state, next_action, united=True)
-        actor_loss = -ReHAE(q_next_target - self.q_next_old_policy)
 
-        self.actor_optimizer.zero_grad()
-        actor_loss.backward()
-        self.actor_optimizer.step()
+        with torch.no_grad():
+            for target_param, param in zip(self.critic_target.parameters(), self.critic.parameters()):
+                target_param.data.copy_(self.tau_*target_param.data + self.tau*param)
+            
+            q_next_target = self.critic_target(next_state, next_action, united=True)
+            q_value = reward +  (1-done) * 0.99 * q_next_target
 
-        #Critic Update
-
-        q_value = reward +  (1-done) * 0.99 * q_next_target.detach()
+       
         qs = self.critic(state, action, united=False)
-        critic_loss = ReHSE(q_value - qs[0]) + ReHSE(q_value - qs[1]) + ReHSE(q_value - qs[2])
+        critic_loss = ReHSE(q_value - qs[0]) + ReHSE(q_value - qs[1]) + ReHSE(q_value - qs[2]) #+ self.lambda_r*cl3_loss
 
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
         self.critic_optimizer.step()
 
 
-        with torch.no_grad():
-            for target_param, param in zip(self.critic_target.parameters(), self.critic.parameters()):
-                target_param.data.copy_(self.tau_*target_param.data + self.tau*param)
-                
-            self.q_next_old_policy = q_next_target.detach().mean()
+        q_next_target_new = self.critic_target(next_state, next_action, united=True)
+        actor_loss = -ReHAE(q_next_target_new - q_next_target)
+
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+
             
 
             
@@ -244,9 +241,9 @@ class Symphony(object):
 
 
 class ReplayBuffer:
-    def __init__(self, state_dim, action_dim, device, fade_factor=7.0, lambda_r=0.02, explore_time=1000):
-        self.capacity, self.length, self.device = 512000, 0, device
-        self.batch_size = min(max(128, self.length//250), 2048) #in order for sample to describe population
+    def __init__(self, state_dim, action_dim, device, capacity, fade_factor=7.0, lambda_r=0.02, explore_time=1000):
+        self.capacity, self.length, self.device = capacity, 0, device
+        self.batch_size = min(max(64, self.length//250), 384) #in order for sample to describe population
         self.random = np.random.default_rng()
         self.indices, self.indexes, self.probs, self.step = [], np.array([]), np.array([]), 0
         self.fade_factor = fade_factor
@@ -272,12 +269,11 @@ class ReplayBuffer:
 
         idx = self.length-1
         
-        
+        """
         #moving is life, stalling is dangerous
         self.rewards_sum += (reward/2)*math.tanh(reward/2)
         self.step += 1
 
-     
         self.alpha = self.alpha_base*(10.0 + self.rewards_sum/self.step)/10.0
         
 
@@ -288,6 +284,7 @@ class ReplayBuffer:
 
         delta /= self.delta_max
         reward += self.alpha*(0.5*math.tanh(math.log(2.0*delta))+0.5)
+        """
         
         
 
@@ -297,7 +294,7 @@ class ReplayBuffer:
         self.next_states[idx,:] = torch.FloatTensor(next_state).to(self.device)
         self.dones[idx,:] = torch.FloatTensor([done]).to(self.device)
 
-        self.batch_size = min(max(128, self.length//250), 2048)
+        self.batch_size = min(max(64, self.length//250), 384)
 
 
         if self.length==self.capacity:
